3cfb1085f4eca38e5eec5801f720215401ceee41
Patel Divyesh <pateldivyesh1323@gmail.com>
2023-08-16 00:36:38 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import {useState} from 'react'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Cart from "./components/Cart";
+import ProductList from "./components/ProductList";
+
+const App = () => {
+  const [viewCart,setViewCart] = useState<boolean>(false);
+  const pageContent = viewCart ? <Cart/> : <ProductList/>
+  const content = (
+    <>
+      <Header viewCart={viewCart} setViewCart={setViewCart} />
+      {pageContent}
+      <Footer viewCart={viewCart}/>
+    </>
+  )
+
+  return content;
+}
+export default App;
